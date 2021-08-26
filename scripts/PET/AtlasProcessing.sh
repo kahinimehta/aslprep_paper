f335d80f197a5e6f9e9e961dbffc5feaf28ef28e
Azeez Adebimpe <azeez.adebimpe@outlook.com>
2021-08-25 22:56:31 -0400

    da

---
+++ scripts/PET/AtlasProcessing.sh
@@ -0,0 +1,2 @@
+
+wb_command -cifti-merge-dense COLUMN all_labels.dlabel.nii -cifti Q1-Q6_RelatedParcellation210.L.CorticalAreas_dil_Colors.32k_fs_LR.dlabel.nii -cifti Q1-Q6_RelatedParcellation210.R.CorticalAreas_dil_Colors.32k_fs_LR.dlabel.nii
